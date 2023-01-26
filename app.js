adafc36cde3d956b4ebd44e37ff5476f4414fa0a
Jadwiga Świerczyńska <jadwiga.swierczynska22@gmail.com>
2023-01-26 13:13:14 +0100

    Initialized file app.js.

---
+++ app.js
@@ -0,0 +1,17 @@
+var http = require( 'http' );
+var express = require( 'express' );
+var pg = require( 'pg' );
+var cookieParser = require('cookie-parser');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use( express.static( "./static" ) );
+app.use(cookieParser());
+
+app.use(express.urlencoded({extended:true}));
+
+http.createServer( app ).listen( 3000 );
+console.log( 'started' );
+
